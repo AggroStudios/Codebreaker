1dc68ac45d6ddbbabb88f4645b8985a8bb0f7c1d
Simon Germain <simon@prth.com>
2025-11-01 01:09:11 -0400

    Removed console log and fixed tests

--- src/lib/processors.test.ts
+++ src/lib/processors.test.ts
@@ -8,14 +8,14 @@ describe('CodiumProcessor', () => {
     expect(proc.flops).toBe(1000000000);
     expect(proc.cores).toBe(4);
     expect(proc.manufacturer).toBe('Codium');
-    expect(proc.model).toBe('Codium 1');
+    expect(proc.model).toBe('Brkr 1');
     expect(proc.speed).toBe('3.2 GHz');
     expect(proc.architecture).toBe(ProcessorArchitecture.risc32);
   });
 
   it('should return correct string from toString()', () => {
     const proc = new CodiumProcessor();
-    expect(proc.toString()).toBe('Codium Codium 1 3.2 GHz (risc32)');
+    expect(proc.toString()).toBe('Codium Brkr 1 (risc32) - 4 cores @ 3.2 GHz');
   });
 });
 
@@ -23,7 +23,7 @@ describe('CodiumProcessor64', () => {
   it('should override properties from CodiumProcessor', () => {
     const proc = new CodiumProcessor64();
     expect(proc.flops).toBe(1300000000);
-    expect(proc.model).toBe('Codium64 1');
+    expect(proc.model).toBe('Brkr64 1');
     expect(proc.architecture).toBe(ProcessorArchitecture.risc64);
     expect(proc.cores).toBe(4); // inherited
     expect(proc.manufacturer).toBe('Codium'); // inherited
@@ -32,6 +32,6 @@ describe('CodiumProcessor64', () => {
 
   it('should return correct string from toString()', () => {
     const proc = new CodiumProcessor64();
-    expect(proc.toString()).toBe('Codium Codium64 1 3.2 GHz (risc64)');
+    expect(proc.toString()).toBe('Codium Brkr64 1 (risc64) - 4 cores @ 3.2 GHz');
   });
 });
